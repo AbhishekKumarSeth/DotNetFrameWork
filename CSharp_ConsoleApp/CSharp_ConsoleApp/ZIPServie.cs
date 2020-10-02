8071cde023a7ce8f2941ed702ec22d292d322cd0
Abhishek Kumar seth <abhishekseth054@gmail.com>
2020-10-02 13:46:09 +0530

    Create and Extract encrypted Zip

---
+++ CSharp_ConsoleApp/CSharp_ConsoleApp/ZIPServie.cs
@@ -0,0 +1,35 @@
+﻿using Ionic.Zip;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_ConsoleApp
+{
+    public class ZIPServie
+    {
+        public static void CreateZip()
+        {
+            string zipPath = @"D:\Temp Doc\Final.zip";
+
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.Password = "ABCD";
+                zip.AddFile(@"D:\Temp Doc\file.csv", "");
+                zip.AddFile(@"D:\Temp Doc\Test.txt", "");
+                zip.Save(zipPath);
+            }
+        }
+
+        public static void ExtratZIPFile()
+        {
+            using (ZipFile zip = ZipFile.Read(@"D:\Temp Doc\Final.zip"))
+            {
+                zip.Password = "ABCD";
+                zip.ExtractAll(@"D:\Temp Doc\test\", ExtractExistingFileAction.DoNotOverwrite);
+                var files = zip.ToList();
+            }
+        }
+    }
+}
